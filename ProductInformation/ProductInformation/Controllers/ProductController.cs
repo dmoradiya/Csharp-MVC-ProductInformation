12e305261694cbb895a438a3a7cb8e73edcc3fa1
dhmec <dharmoradiya@gmail.com>
2020-11-05 12:34:01 -0700

    Add Business Logic in CreateProduct method

--- ProductInformation/ProductInformation/Controllers/ProductController.cs
+++ ProductInformation/ProductInformation/Controllers/ProductController.cs
@@ -29,6 +29,7 @@ namespace ProductInformation.Controllers
                     CreateProduct(categoryID, name);
                     ViewBag.Message = $"Successfully Created Product {name}!";
                 }
+                // Catch ONLY ValidationException. Any other Exceptions (FormatException, DivideByZeroException, etc) will not get caught, and will break the whole program.
                 catch (ValidationException e)
                 {
                     ViewBag.CategoryID = categoryID;
@@ -38,6 +39,8 @@ namespace ProductInformation.Controllers
                     ViewBag.Error = true;
                 }
             }
+
+
 
             ViewBag.Categories = CategoryController.GetCategories();
             return View();
@@ -68,40 +71,86 @@ namespace ProductInformation.Controllers
             ValidationException exception = new ValidationException();
 
             // Trim the values so we don't need to do it a bunch of times later.
-            categoryID = categoryID != null ? categoryID.Trim() : null;
-            name = name != null ? name.Trim() : null;
+            // Common validation points (1) and (4a).
+            categoryID = !string.IsNullOrWhiteSpace(categoryID) ? categoryID.Trim() : null;
+            name = !string.IsNullOrWhiteSpace(name) ? name.Trim() : null;
 
             // Check for individual validation cases and throw an exception if they fail.
             // I'll show you how to bundle up multiple simultaneous exceptions tomorrow.
 
-            // No value for category ID.
-            if (string.IsNullOrWhiteSpace(categoryID))
-            {
-                exception.ValidationExceptions.Add(new Exception("Category ID Not Provided"));
-            }
-            else
-            {
-                // Category ID fails parse.
-                if (!int.TryParse(categoryID, out parsedCategoryID))
-                {
-                    exception.ValidationExceptions.Add(new Exception("Category ID Not Valid"));
-                }
-            }
-
-
-
-            // No value for name.
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                exception.ValidationExceptions.Add(new Exception("Name Not Provided"));
-            }
 
             using (ProductInfoContext context = new ProductInfoContext())
             {
-                if (context.Categories.Where(x => x.ID == parsedCategoryID).Count() < 1)
+
+                //-------------------------
+                // Validation of CategoryID
+                //-------------------------
+
+                // No value for category ID.
+                if (string.IsNullOrWhiteSpace(categoryID))
                 {
-                    exception.ValidationExceptions.Add(new Exception("Category Does Not Exist"));
+                    exception.ValidationExceptions.Add(new Exception("Category ID Not Provided"));
                 }
+                else
+                {
+                    // Category ID fails parse.
+                    // Common validation points (5) and (5a).
+                    if (!int.TryParse(categoryID, out parsedCategoryID))
+                    {
+                        exception.ValidationExceptions.Add(new Exception("Category ID Not Valid"));
+                    }
+                    else
+                    {
+                        // Category ID exists.
+                        // Common validation point (7).
+                        if (!context.Categories.Any(x => x.ID == parsedCategoryID))
+                        {
+                            exception.ValidationExceptions.Add(new Exception("Category Does Not Exist"));
+                        }
+                    }
+                }
+
+                // Validation point (6) doesn't have an example in this solution, but it could be achieved similarly to (7), or by the nature of the Single() method, which will throw an exception if it doesn't exist.
+
+                //-------------------
+                // Validation of Name
+                //-------------------
+
+                // No value for name.
+                // Common validation point (4).
+                if (string.IsNullOrWhiteSpace(name))
+                {
+                    exception.ValidationExceptions.Add(new Exception("Name Not Provided"));
+                }
+                else
+                {
+                    // Name is a duplicate.
+                    // Not a common validation point necessarily, but does perform (2).
+                    if (context.Products.Any(x => x.Name.ToUpper() == name.ToUpper()))
+                    {
+                        exception.ValidationExceptions.Add(new Exception("Name Already Exists"));
+                    }
+                    else
+                    {
+                        if (name.Length > 30)
+                        {
+                            // Name too long
+                            // Common validation point (3).
+                            exception.ValidationExceptions.Add(new Exception("The Maximum Length of a Name is 30 Characters"));
+                        }
+                        else
+                        {
+                            if (name.ToUpper() == "PAPER CUPS" && parsedCategoryID == context.Categories.Where(x => x.Name == "Kitchen").Single().ID)
+                            {
+                                exception.ValidationExceptions.Add(new Exception("Only Glass Glasses Allowed Here"));
+                            }
+                        }
+                    }
+                }
+               
+
+
+
 
                 if (exception.ValidationExceptions.Count > 0)
                 {
